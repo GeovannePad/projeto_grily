066052975ad609e6f70d25b2891cb66e73511b30
GeovannePad <geovannepadilha@hotmail.com>
2018-11-13 14:13:39 -0200

    Páginas

---
+++ pintura.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css-js/css-aditional.css">
+    <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
+    <script src="main.js"></script>
+</head>
+
+<body class="bg">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="">GRILY</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="sobre.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Cursos
+                        </a>
+                        <div class="dropdown-menu colorTree" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="teatro.php">Teatro</a>
+                            <a class="dropdown-item" href="danca.php">Dança</a>
+                            <a class="dropdown-item" href="pintura.php">Pintura</a>
+                            <a class="dropdown-item" href="musica.php">Música</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="midia.php">Mídia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="form.php">Fale conosco</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="banner">
+        <img src="midia/predio-frente2.jpg" class="banner-image">
+        <p class="text-light display-4 centrodois">insira título aqui</p>
+    </div>
+  
+
+<div class="ui grid">
+  <div class="eight wide column"> <img src="https://escolaproarte.com.br/wp-content/uploads/2015/08/marilyn-monroe-88522.jpg" width="75%"></div>
+  <div class="eight wide column"><p>"A arte está presente em nossas vidas todos os dias, em suas diversas formas e manifestações. A palavra arte vem
+                do latim e significa habilidade. O termo pode ser entendido como uma atividade humana ligada à sensibilidade,
+                à criatividade e à imaginação.
+                <br>
+                <br>
+                <em>
+                    <strong>"O que é desenho artístico?" </strong>
+                </em>
+                <br>Nada mais é do que colocar no papel, ou na tela, enfim quaisquer lugares em que queira desenhar, tudo o que
+                sente. Desenhar de maneira artística é não se prender a regras e procedimentos; você pode soltar a imaginação
+                e colocá-la em forma de arte por meio de um desenho. No desenho artístico, você também não precisa se prender
+                a materiais específicos; tem total liberdade para usar o papel, lápis, cores, enfim, tudo o que quiser. Os
+                artistas geralmente colocam em forma de desenho o modo com encaram a realidade e a vontade de desenhar todos
+                os detalhes dentro deste âmbito. O desenho artístico não necessita de regras para ser feito nem de uma linha
+                de criação.
+            </p></div>
+</div>
+
+
+
+
+
+
+    <footer class="colorTwo">
+        <div class="ui center aligned container">
+            <br>
+            <h1>GRILY</h1>
+            <i class="facebook icon"></i>
+            <i class="twitter icon"></i>
+            <i class="instagram icon"></i>
+            <i class="fort awesome icon"> </i>
+            <div class="ui divider"></div>
+            <p>Escola de arte GRILY é uma escola de artes, fundada em 2000 por Marco Nanini, que já está a mais de 40
+                anos no mercado, formando verdadeiros profissionais na arte.
+                <br><br><strong>© Todos os direitos reservados. Fundação GRILY</strong></p>
+            <br>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+        crossorigin="anonymous"></script>
+    <script src="css-js/js-aditional.js"></script>
+    <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <script>
+        $('.special .image').dimmer({
+            on: 'hover'
+        });
+    </script>
+</body>
+
+</html>

14568b2cde4d1d892ddd8c296f0187f592572a55
GeovannePad <geovannepadilha@hotmail.com>
2018-11-13 14:13:27 -0200

    Páginas

---
+++ sobre.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css-js/css-aditional.css">
+    <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
+    <script src="main.js"></script>
+</head>
+
+<body class="bg">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="">GRILY</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="sobre.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Cursos
+                        </a>
+                        <div class="dropdown-menu colorTree" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="teatro.php">Teatro</a>
+                            <a class="dropdown-item" href="danca.php">Dança</a>
+                            <a class="dropdown-item" href="pintura.php">Pintura</a>
+                            <a class="dropdown-item" href="musica.php">Música</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="midia.php">Mídia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="form.php">Fale conosco</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="banner">
+        <img src="midia/predio-frente2.jpg" class="banner-image">
+        <p class="text-light display-4 centrodois">Sobre nós</p>
+    </div>
+  
+    <div class="ui justified container">
+        <br><br>
+        <div class="ui grid">
+            <div class="five wide column">
+                <div class="ui link card">
+  <div class="content">
+    <div class="header">História</div>
+    <div class="meta">
+      <span class="category">quem somos...</span>
+    </div>
+    <div class="description">
+      <p>A Escola de Arte GRILY, foi fundada em 2000. Seus diretores, Marco Nanini, com mais de 40 anos de experiência no
+            mundo da arte, e com seu sucesso nacional de “A Grande Família”, administra hoje a unidade própria em Niterói-RJ,
+            com o cursos técnicos profissionalizantes em danças de rua, ballet, contemporâneo, desenho artístico, ilustração,
+            pintura, artes cênicas e entre outros. <br> No ano de 2002 uma de nossas turmas ganhou em primeiro lugar no concurso regional de expressão corporal como a melhor
+            interpretação grupal. Neste mesmo ano recebeu uma arrecadação da atriz e bailarina Claudia Raia para a criação
+            de um espaço para nosso curso de movimentos rítmicos. Em sua homenagem o local foi nomeado como “Espaço Raia”.<br>O ambiente em todas as unidades é adequado e agradável para o ensino. Você recebe atendimento personalizado dentro
+            do seu ritmo, ou seja, as turmas são formadas com o número máximo de dezoito alunos onde o professor(es) atende
+            às necessidades de cada um individualmente, obtendo assim, um excelente aproveitamento. </p>
+            </div>
+          </div>
+        </div>
+            </div>
+            <div class="eleven wide column">
+                <div class="ui move down reveal">
+                  <div class="visible content">
+                    <img src="https://www.missoulacurrent.com/wp-content/uploads/2017/07/UM.jpg" alt="" width="100%">
+                  </div>
+                  <div class="hidden content">
+                    <img src="https://1.bp.blogspot.com/-3FTOHDUH3Ko/WZA4HBo12yI/AAAAAAAAB04/Vil_lYokERsi9E-Wzm7jF05fxkTGK-HSACLcBGAs/s1600/Massachusetts%2BHall.jpg" width="100%">
+                  </div>
+                </div>
+            
+            </div>
+        </div>
+        <br><br>
+        <p>Na GRILY são formados profissionais de ótimo nível, capacitados para enfrentar com sucesso as exigências do mercado de trabalho. O método de nossa escola é único e garante o desenvolvimento artístico e técnico de seus alunos.
+
+"O Artista não é apenas uma pessoa que trabalha num quartinho de fundo em sua casa, o Artista é aquele que transforma o mundo, mostra o que é belo através das formas, das cores e das várias interpretações que possamos dar as suas obras. O Artista sustenta-se pela arte de alguma forma; lecionando, pintando, desenhando, ilustrando, caricaturando, etc.", e a GRILY têm obtido ótimos resultados investindo na profissionalização do artista. Venha assistir a uma aula gratuita e tire suas próprias conclusões.</p>
+        <br>
+        <div class="ui raised segment">
+            <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit massa id nibh maximus, ut tristique arcu vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque non velit ac sapien rutrum commodo. Quisque pharetra condimentum velit, a pretium sapien ultricies sit amet. Nullam quis tempus urna. Morbi imperdiet odio eu porttitor faucibus. Sed eget ipsum velit. Quisque finibus libero vel quam pellentesque convallis sit amet in mi. Morbi eu metus vel dolor bibendum sodales non eu dui."</em> - Alguém legal</p>
+        </div>
+    </div>
+
+<br><br><br>
+<div class="ui justified container">
+    <h1><i class="address book icon"></i>Informações básicas</h1>
+<table class="ui celled table">
+  <thead>
+    <tr><th>Curso</th>
+    <th>Tempo</th>
+    <th>Disponibilidade</th>
+    <th>Período</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td data-label="pintura">Pintura</td>
+      <td data-label="musica">3 anos</td>
+      <td data-label="teatro"><i class="large green checkmark icon"></i></td>
+      <td data-label="danca">Noturno</td>
+    </tr>
+    <tr>
+     <td data-label="musica">Música</td>
+      <td data-label="Age">5 anos</td>
+      <td data-label="Job"><i class="large green checkmark icon"></i></td>
+      <td data-label="Job">Diurno</td>
+    </tr>
+    <tr>
+      <td data-label="teatro">Teatro</td>
+      <td data-label="Age">100 anos</td>
+      <td data-label="Job"><i class="large green checkmark icon"></i></td>
+      <td data-label="Job">Diurno</td>
+    </tr>
+    <tr>
+       <td data-label="danca">Dança</td>
+      <td data-label="Age">1 ano</td>
+      <td data-label="Job"><i class="large green checkmark icon"></i></td>
+      <td data-label="Job">Diurno</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<br><br><br>
+    <footer class="colorTwo">
+        <div class="ui center aligned container">
+            <br>
+            <h1>GRILY</h1>
+            <i class="facebook icon"></i>
+            <i class="twitter icon"></i>
+            <i class="instagram icon"></i>
+            <i class="fort awesome icon"> </i>
+            <div class="ui divider"></div>
+            <p>Escola de arte GRILY é uma escola de artes, fundada em 2000 por Marco Nanini, que já está a mais de 40
+                anos no mercado, formando verdadeiros profissionais na arte.
+                <br><br><strong>© Todos os direitos reservados. Fundação GRILY</strong></p>
+            <br>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+        crossorigin="anonymous"></script>
+    <script src="css-js/js-aditional.js"></script>
+    <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <script>
+        $('.special .image').dimmer({
+            on: 'hover'
+        });
+    </script>
+</body>
+
+</html>

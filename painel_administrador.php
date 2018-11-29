6330f9335ee9af495a6f283408190b671e250286
GeovannePad <geovannepadilha@hotmail.com>
2018-11-28 22:47:26 -0200

    Fazendo o filtro de usuarios

--- painel_administrador.php
+++ painel_administrador.php
@@ -9,12 +9,61 @@
   <link rel="stylesheet" href="css-js/css-aditional.css">
 </head>
 <body>
-  <script src="css-js/"></script>
+  <div class="ui grid container stackable">
+    <div class="sixteen wide column">
+      <div class="ui center aligned segment">
+        <h4 class=>Painel do administrador, seja bem vindo (session)</h4>
+      </div>
+    </div>
+    <div class="ui vertical segment">
+      <div class="two wide column">
+        <div class="ui floating labeled icon dropdown button" id="filtroDropdown">
+          <i class="filter icon"></i>
+          <span class="text">Filter</span>
+          <div class="menu">
+            <div class="header">
+              <i class="tags icon"></i>
+              Filter by tag
+            </div>
+            <div class="divider"></div>
+            <div class="item">
+              <i class="attention icon"></i>
+              Important
+            </div>
+            <div class="item">
+              <i class="comment icon"></i>
+              Announcement
+            </div>
+            <div class="item">
+              <i class="conversation icon"></i>
+              Discussion
+            </div>
+          </div>
+        </div>
+        
+      </div> 
+      </div>
+      <div class="twelve wide column">
+        
+      </div>
+      <div class="two wide column">
+
+      </div>
+    </div>
+  </div>
+  
+  <script src="css-js/jquery-3.3.1.min.js"></script>
+  <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+  <script src="css-js/js-aditional.js"></script>
+  <script>
+    $("#filtroDropdown")
+    .dropdown();
+  </script>
 </body>
 </html>
 <?php 
 
-require_once("config.php");
+/*require_once("config.php");
 $usuarios = new Usuario();
 if (isset($_GET["tipo"])) {
   $tipo = $_GET["tipo"];
@@ -41,4 +90,5 @@ foreach ($resultados as $resultado) {
   }
   echo "</tr>";
 }
+*/
 ?>

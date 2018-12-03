533567f1eba96bf1d74c561f82c1e86c8e9f0197
Geovanne Padilha <geovannepadilha@hotmail.com>
2018-12-02 23:22:45 -0200

    Listagem e sign out feitos, falta fazer o modal para os dados de usuário dos estudantes

--- painel_administrador.php
+++ painel_administrador.php
@@ -9,47 +9,74 @@
   <link rel="stylesheet" href="css-js/css-aditional.css">
 </head>
 <body>
+  <?php
+    require_once("config.php");
+    if (!isset($_SESSION["administrador"])) {
+      header("Location: plogin.php");
+    }
+  ?>
   <div class="ui grid container stackable">
     <div class="sixteen wide column">
       <div class="ui center aligned segment">
-        <h4 class=>Painel do administrador, seja bem vindo (session)</h4>
+        <h4 class=>Painel do administrador, seja bem vindo <?=$_SESSION["administrador"]["nome"]?></h4>
       </div>
     </div>
-    <div class="ui vertical segment">
-      <div class="two wide column">
-        <div class="ui floating labeled icon dropdown button">
-          <i class="filter icon"></i>
-          <span class="text">Filtro</span>
-          <div class="menu">
-            <div class="header">
-              <i class="tags icon"></i>
-              Usuário
-            </div>
-            <div class="divider"></div>
-            <div class="item">
-              <i class="attention icon"></i>
-              Administradores
-            </div>
-            <div class="item">
-              <i class="comment icon"></i>
-              Organizadores
-            </div>
-            <div class="item">
-              <i class="conversation icon"></i>
-              Estudantes
-            </div>
+    <div class="sixteen wide column">
+      <div class="ui floating labeled icon dropdown button left floated">
+        <i class="filter icon"></i>
+        <span class="text">Filtro</span>
+        <div class="menu">
+          <div class="header">
+            <i class="tags icon"></i>
+            Selecionar usuários
+          </div>
+          <div class="divider"></div>
+          <div class="item">
+            <i class="attention icon"></i>
+            Administradores
+          </div>
+          <div class="item">
+            <i class="comment icon"></i>
+            Organizadores
+          </div>
+          <div class="item">
+            <i class="conversation icon"></i>
+            Estudantes
           </div>
         </div>
-        <button class="ui positive button" id="filterButton">Filtrar</button>
       </div>
-        
-      </div> 
-      </div>
-      <div class="twelve wide column">
-        
-      </div>
-      <div class="two wide column">
+      <button class="ui positive button left floated" id="filterButton">Filtrar</button>
+      <button class="ui negative button right floated" id="signOutButton">Deslogar</button>
+      <button class="ui primary button right floated" id="addUserButton">Adicionar Usuário</button>
+    </div>
+    <div class="sixteen wide column">
+      <table class="ui unstackable table">
+        <?php
+          require_once("listagem.php");
+        ?>
+      </table>
+      
+    </div>
+  </div>
 
+  <!-- Janela Modais -->
+  <div class="ui modal">
+    <i class="close icon"></i>
+    <div class="header">
+      Perfil do estudante 
+    </div>
+    <div class="image content">
+      <div class="ui medium image">
+        <img src=<?=$url?>>
+      </div>
+      <div class="description">
+        <div class="ui header">Dados</div>
+          
+        </div>
+      </div>
+      <div class="actions">
+      <div class="ui black deny button">
+        Sair
       </div>
     </div>
   </div>
@@ -63,41 +90,21 @@
       $("#filterButton").click(function(){
         var userType = $(".ui.icon").dropdown('get text').trim().toLowerCase();
         var type = userType.toLowerCase();
-        window.location.href = "http://localhost/actions.php?action=selecionar_usuario&tipo=" + type;
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=selecionar_usuario&tipo=" + type;
+      });
+      $("#addUserButton").click(function(){
+        window.location.href = "http://localhost/projeto_grily/pregistro.php";
       }); 
+      $("#signOutButton").click(function(){
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=deslogar"
+      });
+      $('.ui.accordion').accordion();
+      $('#userAccount').click(function(){
+        var id = $('#userAccount').attr("value");
+        alert(id);
+      });
     });
     
   </script>
 </body>
 </html>
-<?php 
-
-/*require_once("config.php");
-$usuarios = new Usuario();
-if (isset($_GET["tipo"])) {
-  $tipo = $_GET["tipo"];
-} else {
-  $tipo = "Administrador";
-}
-$resultados = $usuarios->selectUsuariosByTipo($tipo);
-foreach ($resultados as $resultado) {
-  echo "<tr>";
-  foreach ($resultado as $key => $value) {
-    echo "<td>";
-    if ($key == "dtregistro") {
-      $data = new DateTime($value);
-      $value = $data->format("d-m-Y");
-      echo $value;
-      continue;
-    }
-    if ($key == "idusuario") {
-      echo "<a class='btn btn-danger' href='actions.php?action=apagar_usuario&idusuario=$value' role='button'>Excluir</a>";
-      continue;
-    }
-    echo $value;
-    echo "</td>";
-  }
-  echo "</tr>";
-}
-*/
-?>

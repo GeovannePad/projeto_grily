2cf2b0298c319a65ace148d88bb393bda4e5b8ed
Geovanne Padilha <geovannepadilha@hotmail.com>
2018-12-03 23:22:19 -0200

    Conserto de bugs e preview dos dados feitos (everything working)

--- painel_administrador.php
+++ painel_administrador.php
@@ -10,7 +10,6 @@
   <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
   <link rel="stylesheet" href="css-js/css-aditional.css">
 </head>
-<body>
   <?php
     require_once("config.php");
     if (!isset($_SESSION["administrador"])) {
@@ -23,7 +22,7 @@
   <div class="ui grid container stackable">
     <div class="sixteen wide column">
       <div class="ui center aligned segment">
-        <h4 class=>Painel do administrador, seja bem vindo <?=$_SESSION["administrador"]["nome"]?></h4>
+        <h4>Painel do administrador, seja bem vindo <?=$_SESSION["administrador"]["nome"]?></h4>
       </div>
     </div>
     <div class="sixteen wide column">
@@ -57,29 +56,50 @@
     <div class="sixteen wide column">
       <table class="ui unstackable table">
         <?php
-          require_once("listagem.php");
+          require_once("listagem_administrador.php");
         ?>
       </table>
       
     </div>
   </div>
 
+  <?php
+    if (isset($_GET["preview_dados"]) && $_GET["preview_dados"] == "sim") {
+      $idusuario = $_GET["idusuario"];
+      $usuario = new Usuario();
+      $dados = $usuario->selectUsuarioEstudanteById($idusuario);
+      $dataConvert = new DateTime($dados[0]["dtregistro"]);
+      $dataConvertida = $dataConvert->format("d-m-Y");
+      $dados[0]["dtregistro"] = $dataConvertida;
+      if ($dados[0]["imagem"] == "user-icon.png") {
+        $_SESSION["imagemEstudante"] = "midia/" . $dados[0]["imagem"];
+      } else {
+        $_SESSION["imagemEstudante"] = "midia/imagens_estudantes/" . $dados[0]["imagem"];
+      } 
+      $dadosTratados = json_encode($dados);      
+      echo "<script>var jsonDados = new Array(); jsonDados = $dadosTratados</script>";
+    }
+  ?>
+    
   <!-- Janela Modais -->
   <div class="ui modal">
+    <div id="dadosPHP" style="display:none;"><?=$dadosTratados?></div>
     <i class="close icon"></i>
     <div class="header">
-      Perfil do estudante 
+      Dados do estudante
     </div>
     <div class="image content">
-      <div class="ui medium image">
-        <img src=<?=$url?>>
+      <div class="ui medium circular image">
+        <img src=<?= $_SESSION["imagemEstudante"] ?> alt="User perfil image" id="imagemEstudante">
       </div>
       <div class="description">
         <div class="ui header">Dados</div>
-          
-        </div>
+        <h4 id="nomeEstudante">Nome:  </h4>
+        <h4 id="loginEstudante">Login:  </h4>
+        <h4 id="dtregistroEstudante">Data de registro:  </h4>
       </div>
-      <div class="actions">
+    </div>
+    <div class="actions">
       <div class="ui black deny button">
         Sair
       </div>
@@ -91,6 +111,14 @@
   <script src="css-js/js-aditional.js"></script>
   <script>
     $(document).ready(function(){
+      <?php
+        if (isset($_GET["preview_dados"]) && $_GET["preview_dados"] == "sim") {
+          echo " $('#nomeEstudante').append(jsonDados[0].nome);";
+          echo " $('#loginEstudante').append(jsonDados[0].login);";
+          echo " $('#dtregistroEstudante').append(jsonDados[0].dtregistro);";
+          echo " $('.ui.modal').modal('show');";        
+        }
+      ?>
       $(".ui.icon").dropdown();
       $("#filterButton").click(function(){
         var userType = $(".ui.icon").dropdown('get text').trim().toLowerCase();
@@ -101,15 +129,17 @@
         window.location.href = "http://localhost/projeto_grily/pregistro.php";
       }); 
       $("#signOutButton").click(function(){
-        window.location.href = "http://localhost/projeto_grily/actions.php?action=deslogar"
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=deslogar";
       });
       $('.ui.accordion').accordion();
-      $('#userAccount').click(function(){
-        var id = $('#userAccount').attr("value");
-        alert(id);
+      $('.userAccount').click(function(){
+        // var id = $('.userAccount').attr("value");
+        // window.location.href = "http://localhost/projeto_grily/painel_administrador.php?dados_estudante="+ id;
+      });
+      $('.ui.black.deny.button').click(function(){
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=selecionar_usuario&tipo=estudantes";
       });
     });
-    
   </script>
 </body>
 </html>

d685d6f817b1cde695d3765f88affee270e3cec3
Geovanne Padilha <geovannepadilha@hotmail.com>
2018-12-03 23:22:58 -0200

    Colocado em semantic ui e ações especias concluidas (everything working)

--- painel_organizador.php
+++ painel_organizador.php
@@ -7,68 +7,139 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
+    <link rel="stylesheet" href="css-js/css-aditional.css">
   </head>
+  <?php
+    require_once("config.php");
+    if (!isset($_SESSION["organizador"])) {
+      header("Location: plogin.php");
+    }
+    if (isset($_GET["confirmar_inscricao"]) && $_GET["confirmar_inscricao"] == "sim") {
+      header("Location: actions.php?action=registrar_estudante");
+    } else if (isset($_GET["confirmar_inscricao"]) && $_GET["confirmar_inscricao"] == "nao") {
+      unset($_SESSION["dados_inscricao"]);
+      header("Location: painel_organizador.php");
+    }
+    if (isset($_GET["gerar_preview"]) && $_GET["gerar_preview"] == "sim") {
+      $inscricao = new Inscricao();
+      $dados = $inscricao->selectInscricaoById($_GET["idinscricao"]);
+      $dados[0]["login"] = gerarLoginEstudante($dados[0]["nome"]);
+      $dados[0]["rm"] = mt_rand(11111, 99999);
+      $dateConvert = new DateTime($dados[0]["dtnascimento"]);
+      $dados[0]["dtnascimento"] = $dateConvert->format("d/m/Y");
+      if ($dados[0]["idcurso"] == 1) {
+        $dados[0]["idcurso"] = "Teatro"; 
+      } else if ($dados[0]["idcurso"] == 2){
+        $dados[0]["idcurso"] = "Dança";
+      } else if ($dados[0]["idcurso"] == 3){
+        $dados[0]["idcurso"] = "Música";
+      } else {
+        $dados[0]["idcurso"] = "Artes";
+      }
+      $dadosTratados = json_encode($dados[0]);
+      $_SESSION["dados_inscricao"] = $dados[0];
+      $_SESSION["dados_inscricao"]["idinscricao"] = $_GET["idinscricao"];
+      echo "<script>var jsonDados = new Array(); jsonDados = $dadosTratados</script>";
+    }
+  ?>
   <body>
-    <div class="container">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Data de nascimento</th>
-            <th>Endereço</th>
-            <th>Telefone</th>
-            <th>Curso</th>
-            <th>Biografia</th>
-          </tr>        
-        </thead>
-        <tbody>
+    <div class="ui grid container stackable">
+      <div class="sixteen wide column">
+        <div class="ui center aligned segment">
+          <h4>Painel do organizador, seja bem vindo <?= $_SESSION["organizador"]["nome"] ?></h4>
+        </div>
+      </div>
+      <div class="sixteen wide column">
+        <div class="ui floating labeled icon dropdown button left floated">
+          <i class="filter icon"></i>
+          <span class="text">Filtro</span>
+          <div class="menu">
+            <div class="header">
+              <i class="tags icon"></i>
+              Selecionar usuários
+            </div>
+            <div class="divider"></div>
+            <div class="item">
+              <i class="attention icon"></i>
+              Inscrições
+            </div>
+            <div class="item">
+              <i class="comment icon"></i>
+              Estudantes
+            </div>
+          </div>
+        </div>
+        <button class="ui positive button left floated" id="filterButton">Filtrar</button>
+        <button class="ui negative button right floated" id="signOutButton">Deslogar</button>
+      </div>
+      <div class="sixteen wide column">
+        <table class="ui unstackable table">
           <?php
-            require_once("config.php");
-            $inscricoes = new Inscricao();
-            $resultados = $inscricoes->selectAllInscricoes();
-            foreach ($resultados as $resultado) {
-              echo "<tr>";
-              foreach ($resultado as $key => $value) {
-                echo "<td>";
-                if ($key == "idcurso") {
-                  if ($value == 1) {
-                    echo "Teatro";
-                  } else if ($value == 2){
-                    echo "Dança";
-                  } else if ($value == 3){
-                    echo "Música";
-                  } else {
-                    echo "Artes";
-                  }
-                  continue;
-                }
-                if ($key == "dtnascimento") {
-                  $data = new DateTime($value);
-                  $value = $data->format("d-m-Y");
-                  echo $value;
-                  continue;
-                }
-                if ($key == "idinscricao") {
-                  echo "<a class='btn btn-success' href='actions.php?action=registrar_estudante&idinscricao=$value' role='button'>Aprovar</a> ";
-                  echo "<a class='btn btn-danger' href='actions.php?action=apagar_inscricao&idinscricao=$value' role='button'>Negar</a>";
-                  continue;
-                }
-                echo $value;
-                echo "</td>";
-              }
-              echo "<tr>";
-            }
-            
+            require_once("listagem_organizador.php");
           ?>
-        </tbody>
-      </table>
-      <a class="btn btn-primary" href="painel_estudantes.php" role="button">Painel dos estudantes</a>
+        </table>
+      </div>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+
+    <!-- Janelas modais -->
+    <div class="ui modal">
+      <i class="close icon"></i>
+      <div class="header">
+        Preview dos dados a serem criados do estudante selecionado!
+      </div>
+      <div class="content">
+        <h4 id="rmEstudante">RM:  </h4>
+        <h4 id="loginEstudante">Login:  </h4>
+        <h4 id="nomeEstudante">Nome:  </h4>
+        <h4 id="datanascimentoEstudante">Data de nascimento:  </h4>
+        <h4 id="cursoEstudante">Curso:  </h4>
+        <h4 id="foneEstudante">Fone:  </h4>
+        <h4 id="enderecoEstudante">Endereço:  </h4>
+        <h4 id="biografiaEstudante">Biografia:  </h4>
+      </div>
+      <div class="actions">
+        <div class="ui buttons">
+          <button class="ui positive button" id="confirmarInscricao">Confirmar</button>
+          <button class="ui negative button" id="cancelarInscricao">Cancelar</button>  
+        </div>
+      </div>
+    </div>
+
+    <script src="css-js/jquery-3.3.1.min.js"></script>
+    <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <script src="css-js/js-aditional.js"></script>
+    <script>
+      $(".ui.icon").dropdown();
+      $('.ui.accordion').accordion();
+
+      $("#filterButton").click(function(){
+        var userType = $(".ui.icon").dropdown('get text').trim().toLowerCase();
+        var type = userType.toLowerCase();
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=selecionar_usuario&tipo=" + type;
+      });
+      $("#signOutButton").click(function(){
+        window.location.href = "http://localhost/projeto_grily/actions.php?action=deslogar";
+      });
+      $("#confirmarInscricao").click(function(){
+        window.location.href = "http://localhost/projeto_grily/painel_organizador.php?confirmar_inscricao=sim";
+      });
+      $("#cancelarInscricao").click(function(){
+        window.location.href = "http://localhost/projeto_grily/painel_organizador.php?confirmar_inscricao=nao";
+      });
+    <?php
+      if (isset($_GET["gerar_preview"]) && $_GET["gerar_preview"] == "sim") {
+        echo " $('#rmEstudante').append(jsonDados.rm);";
+        echo " $('#loginEstudante').append(jsonDados.login);";
+        echo " $('#nomeEstudante').append(jsonDados.nome);";
+        echo " $('#cursoEstudante').append(jsonDados.idcurso);";
+        echo " $('#foneEstudante').append(jsonDados.fone);";
+        echo " $('#enderecoEstudante').append(jsonDados.endereco);";
+        echo " $('#biografiaEstudante').append(jsonDados.biografia);";
+        echo " $('#datanascimentoEstudante').append(jsonDados.dtnascimento);";
+        echo " $('.ui.modal').modal('show');";
+      }
+    ?>
+    </script>
   </body>
 </html>
